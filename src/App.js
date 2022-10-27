cb1ad4fa975cc2a4856e296b59315033e7ae1754
Nila Homar <nilahomar@gmail.com>
2022-10-27 20:28:31 +0200

    directory component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import CategoryItem from './componets/category-item/category-item.component';
-import './categories.styles.scss';
+import Directory from "./components/directory/directory.component";
 
 
 const App = () => {
@@ -30,11 +29,9 @@ const App = () => {
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
   ]
-  return <div className="categories-container">
-    {categories.map((category) => (
-      <CategoryItem key={category.id} category={category} />
-    ))}
-  </div>;
+  return (
+    <Directory categories={categories} />
+  )
 };
 
 export default App;
